df8a76a9bccf39e15b806f813cce59c9e10bda56
shahinul <shahinul37@outlook.com>
2022-04-03 11:06:49 +0600

    auto typed text added

---
+++ main.js
@@ -0,0 +1,14 @@
+//auto typing
+const text = document.getElementById("auto-typed");
+const prog = text.innerText;
+let idx = 0;
+
+setInterval(writeText, 300);
+
+function writeText() {
+  text.innerText = prog.slice(0, idx);
+  idx++;
+  if (idx > prog.length) {
+    idx = 1;
+  }
+}
